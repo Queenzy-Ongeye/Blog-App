d4f06032ca2ae482aed002a0dd3596bef9e73429
Queenzy-Ongeye <queenzyblack176@gmail.com>
2024-05-13 15:33:49 +0300

    added crud methods and functions

--- src/blog/blog.service.ts
+++ src/blog/blog.service.ts
@@ -1,4 +1,38 @@
 import { Injectable } from '@nestjs/common';
-
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Blog, BlogDocument } from './blog.schema';
+import { CreateBlogDto } from './dtos/create.dto';
+import { UpdateBlogDto } from './dtos/update.dto';
 @Injectable()
-export class BlogService {}
+export class BlogService {
+    constructor(@InjectModel(Blog.name) private BlogModel: Model<BlogDocument>) { }
+    // Creating a blog post
+    async create(createBlogDto: CreateBlogDto): Promise<Blog> {
+        const blog = new this.BlogModel(createBlogDto);
+        return await blog.save()
+    }
+
+    // Updating a specific blog post
+    async update(updateBlogDto: UpdateBlogDto): Promise<Blog> {
+        return this.BlogModel.findOneAndUpdate(
+            { _id: updateBlogDto._id, },
+            updateBlogDto.clientPrps,
+            { new: true }
+        )
+    }
+
+    // Getting one blog
+    async getOne(id: string): Promise<Blog>{
+        return await this.BlogModel.findOne({ _id: id})
+    }
+    // Getting all blogs
+    async getAll(): Promise<Blog[]>{
+        return await this.BlogModel.find({})
+    }
+
+    // Deleting one blog
+    async deleteOne(id: string){
+        return this.BlogModel.findOneAndReplace({_id: id})
+    }
+}
