927a94b6fb63cf69ded1af73681b0fca7550f83b
Queenzy-Ongeye <queenzyblack176@gmail.com>
2024-05-13 15:40:47 +0300

    added a createBlog fucntion

--- src/blog/blog.controller.ts
+++ src/blog/blog.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
-
+import { Body, Controller, Post } from '@nestjs/common';
+import { BlogService } from './blog.service';
+import { CreateBlogDto } from './dtos/create.dto';
 @Controller('blog')
-export class BlogController {}
+export class BlogController {
+    constructor(private blogService: BlogService) { }
+    
+    @Post("create")
+    async create(@Body() body: CreateBlogDto){
+        return await this.blogService.create(body);
+    }
+}
