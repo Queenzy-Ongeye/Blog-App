5c75c69d5caef6c835e9920e65084d195f1ed11d
Queenzy-Ongeye <queenzyblack176@gmail.com>
2024-05-13 14:57:29 +0300

    Added Mongoose module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { UserService } from './user/user.service';
+import { UserController } from './user/user.controller';
+import { BlogService } from './blog/blog.service';
+import { BlogController } from './blog/blog.controller';
+import { Blog, BlogSchema } from './blog/blog.schema';
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [MongooseModule.forRoot(process.env.MONGODB_URI),
+    ConfigModule.forRoot({isGlobal: true, envFilePath: ".env"}),
+    MongooseModule.forFeature([{name: Blog.name, schema: BlogSchema}])
+  ],
+  controllers: [AppController, UserController, BlogController],
+  providers: [AppService, UserService, BlogService],
 })
 export class AppModule {}
