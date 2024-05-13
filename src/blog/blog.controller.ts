e9164e8d8b245eb1f10feed5670225836ec76dd7
Queenzy-Ongeye <queenzyblack176@gmail.com>
2024-05-13 15:53:54 +0300

    Added route controller functions

--- src/blog/blog.controller.ts
+++ src/blog/blog.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { BlogService } from './blog.service';
 import { CreateBlogDto } from './dtos/create.dto';
+import { UpdateBlogDto } from './dtos/update.dto';
 @Controller('blog')
 export class BlogController {
     constructor(private blogService: BlogService) { }
@@ -8,5 +9,25 @@ export class BlogController {
     @Post("create")
     async create(@Body() body: CreateBlogDto){
         return await this.blogService.create(body);
+    }
+
+    @Post("update")
+    async update(@Body() body:UpdateBlogDto){
+        return await this.blogService.update(body);
+    }
+
+    @Get("post/:id")
+    async getOne(@Param('id') id: string){
+        return await this.blogService.getOne(id);
+    }
+
+    @Get("all")
+    async getAll(){
+        return await this.blogService.getAll();
+    }
+
+    @Delete("delete/:id")
+    async delete(@Param('id') id: string){
+        return await this.blogService.deleteOne(id);
     }
 }
