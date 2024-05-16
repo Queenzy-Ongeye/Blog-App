4065209ac6aa7105bc85e05d8fcc59016ccce9ba
Queenzy-Ongeye <queenzyblack176@gmail.com>
2024-05-16 17:16:52 +0300

    added user prop

--- src/blog/blog.schema.ts
+++ src/blog/blog.schema.ts
@@ -1,13 +1,16 @@
 import { Schema, Prop, SchemaFactory } from "@nestjs/mongoose";
-import { HydratedDocument } from "mongoose";
+import mongoose, { HydratedDocument } from "mongoose";
+import { User } from "src/user/user.schema";
 
 export type BlogDocument = HydratedDocument<Blog>;
 @Schema()
-export class Blog{
-    @Prop({required: true})
+export class Blog {
+    @Prop({ required: true })
     title: string;
-    @Prop({required: true})
+    @Prop({ required: true })
     content: string;
+    @Prop({ required: true, type: mongoose.Schema.Types.ObjectId, ref: 'User' })
+    user: User
 }
 
 export const BlogSchema = SchemaFactory.createForClass(Blog); //setting up blog schema
